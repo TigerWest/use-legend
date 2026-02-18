dead7de8b7e8f64100541ab6a04af2fc75d9a4c8
tigerwest <sidejump2@gmail.com>
2026-02-18 23:10:33 +0900

    chore: modify tsup

--- packages/integrations/tsup.config.ts
+++ packages/integrations/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/**/*.{ts,tsx}', '!src/**/__tests__/**', '!src/**/*.spec.*'],
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
   clean: true,
-  splitting: false,
+  bundle: false,
 });
