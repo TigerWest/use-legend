f067858fdd2adb01035f82c0dc0835d52818e900
tigerwest <sidejump2@gmail.com>
2026-03-17 18:06:30 +0900

    chore: update build memory size, build exclude spec files

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ['src/**/*.{ts,tsx}', '!src/**/*.spec.*'],
   format: ['esm', 'cjs'],
   dts: true,
+  tsconfig: 'tsconfig.build.json',
   sourcemap: true,
   clean: true,
   bundle: false,
